ff1f07b7e5f6f4be5224d42bae3dbb70cc41e288
wangc <wangc@github.com>
2021-07-16 16:48:25 +0800

    更改配置

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require("./dist/pvuv.min.js");
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-	entry: "./src/index.js",
+	entry: "./lib/index.js",
 	output: {
 		filename: "pvuv.min.js",
 		path: path.resolve(__dirname, "dist"),
